f6875509322d5173d4213420b9d41d7f88f7c337
@ROCKMURPHY <61891464+christcame@users.noreply.github.com>
2024-09-07 13:35:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import os
+from mistralai import Mistral, UserMessage, SystemMessage
 
+token = os.environ["GITHUB_TOKEN"]
+endpoint = "https://models.inference.ai.azure.com"
+model_name = "Mistral-large-2407"
+
+client = Mistral(api_key=token, server_url=endpoint)
+
+response = client.chat.complete(
+    model=model_name,
+    messages=[
+        SystemMessage(content="You are a helpful assistant."),
+        UserMessage(content="What is the capital of France?"),
+    ],
+    temperature=1.,
+    max_tokens=1000,
+    top_p=1.
+)
+
+print(response.choices[0].message.content)
